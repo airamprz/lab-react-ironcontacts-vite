c6b83725b0d8dbb57e76c833f2a893428f8f72dd
airamprz <airamprz509@gmail.com>
2023-10-24 15:52:03 +0100

    [PTREMOTEES] - Airam Pérez

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,76 @@
+import React, { useState } from "react";
 import "./App.css";
+import contactsData from "./contacts.json";
 
 function App() {
+  const allContacts = contactsData;
+  const [contacts, setContacts] = useState(allContacts.slice(0, 5));
+
+  const addRandomContact = () => {
+    const remainingContacts = allContacts.filter(
+      (contact) => !contacts.includes(contact)
+    );
+
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+    const randomContact = remainingContacts[randomIndex];
+
+    setContacts([...contacts, randomContact]);
+  };
+
+  const sortByName = () => {
+    const sortedContacts = [...contacts].sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setContacts(sortedContacts);
+  }
+
+  const sortByPopularity = () => {
+    const sortedContacts = [...contacts].sort((a, b) => b.popularity - a.popularity);
+    setContacts(sortedContacts);
+  }
+
+  const removeContact = (id) => {
+    const filteredContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(filteredContacts);
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by Name</button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Popularidad</th>
+            <th>Ganó un Oscar</th>
+            <th>Ganó un Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  className="contact-img"
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              <td>{contact.wonOscar ? "🏆" : null}</td>
+              <td>{contact.wonEmmy ? "🌟" : null}</td>
+              <td>
+                <button onClick={() => removeContact(contact.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
